0739775d10a1e21471e7272f2393fa51694e94de
EdR668 <ehernandezbu@unal.edu.co>
2025-01-05 17:55:16 -0500

    Fix mongo

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from "./app"
 import mongoose from "mongoose";
 import env from "./utils/validateEnv";
 
-const port = env.PORT;
-mongoose.connect(env.MONGODB_URI)
+const port = env.PORT_ID;
+mongoose.connect(env.MONGODB_URI_ID)
   .then(() => {
     console.log("Mongoose connected") 
     app.listen(port, () => {
