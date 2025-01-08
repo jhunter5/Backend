a094da373358f905cc830ce7a17fb298f0cc58b6
EdR668 <ehernandezbu@unal.edu.co>
2025-01-07 21:19:25 -0500

    [Fix] buffer

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(
   cors());
 
-app.use(fileUpload({useTempFiles:true, tempFileDir: "./uploads/"}));
+  app.use(fileUpload({ useTempFiles: false }));
 
 app.use(morgan("dev"));
 
